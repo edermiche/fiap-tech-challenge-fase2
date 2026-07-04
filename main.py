44184ffd3065b3520684585818a2c127d6e70604
edermiche <40071982+edermiche@users.noreply.github.com>
2026-07-04 10:07:32 -0300

    adiciona processamento silver

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from src.bronze.processar_bronze import processar_camada_bronze
+from src.silver.processar_silver import processar_camada_silver
 
 
 def main() -> None:
     processar_camada_bronze()
+    processar_camada_silver()
 
 
 if __name__ == "__main__":
